cb298a335dee1b27520e0098a423bd94423cde68
gassdev <gassdev@gmail.com>
2020-12-06 23:10:15 +0000

    User Authentication Endpoint

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+
+// @desc Auth user & get token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        })
+    } else {
+        res.status(401)
+        throw new Error('Email et/ou mot de passe invalide(s)')
+    }
+})
+
+export {
+    authUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -13,14 +14,16 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.use(express.json())
 
 app.use('/api/categories', categoryRoutes)
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
