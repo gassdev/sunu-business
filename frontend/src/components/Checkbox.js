217e365ebcdcf95da239d4718b98d5c965ffe312
gassdev <gassdev@gmail.com>
2020-12-20 00:03:28 +0000

    handle categories toggle, passing categories filter to parent component and set filters with category

--- frontend/src/components/Checkbox.js
+++ frontend/src/components/Checkbox.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-bootstrap'
 
-const Checkbox = ({ categories }) => {
+const Checkbox = ({ categories, handleFilters }) => {
+
+    const [checked, setChecked] = useState([])
+
+    const handleToggle = c => () => {
+        // return the first index or -1
+        const currentCategoryId = checked.indexOf(c)
+        const newCheckedCategoryId = [...checked]
+
+        // if currently checked was not already in checked state > push
+        // else pull/take off
+        if (currentCategoryId === -1) {
+            newCheckedCategoryId.push(c)
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1)
+        }
+
+        // console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId)
+
+        handleFilters(newCheckedCategoryId)
+    }
+
     return categories.map((c, i) => (
         <li key={i} className="list-unstyled">
             <Form.Check
                 type="checkbox"
                 label={c.name}
+                id={c.name}
+                value={checked.indexOf(c._id) === -1}
+                onChange={handleToggle(c._id)}
             ></Form.Check>
         </li>
     ))
--- frontend/src/pages/ShopPage.js
+++ frontend/src/pages/ShopPage.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { listCategories } from '../actions/categoryActions'
 import Checkbox from '../components/Checkbox'
 
 const ShopPage = () => {
+
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    })
 
     const dispatch = useDispatch()
 
@@ -15,16 +19,26 @@ const ShopPage = () => {
         dispatch(listCategories())
     }, [dispatch])
 
+    const handleFilters = (filters, filterBy) => {
+        // console.log('SHOP PAGE', filters, filterBy)
+        const newFilters = { ...myFilters }
+        newFilters.filters[filterBy] = filters
+        setMyFilters(newFilters)
+    }
+
     return (
         <>
             <Row>
                 <Col lg={4}>
                     <h4>Filter par catégories</h4>
                     <ul>
-                        <Checkbox categories={categories} />
+                        <Checkbox
+                            categories={categories}
+                            handleFilters={filters => handleFilters(filters, "category")}
+                        />
                     </ul>
                 </Col>
-                <Col lg={8}>right</Col>
+                <Col lg={8}>{JSON.stringify(myFilters)}</Col>
             </Row>
         </>
     )
